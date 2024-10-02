1cf87dc0bdab0327715d98d182fc77aefc0aa554
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-10-02 15:03:34 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
 
 export default class App extends Component {
   render() {
-    const name="sanidhya varshney";
+    
     return (
-      <div><h1>
-        Hello this is {name},a software developer
-        </h1>
-        </div>
+      <>
+         <Navbar />
+      </>
     )
   }
 }
